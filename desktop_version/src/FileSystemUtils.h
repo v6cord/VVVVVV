82b42bfa9349f24281cfb3e40c79684904762582
Dav999-v <dav999.tolp@gmail.com>
2020-01-12 15:17:39 +0100

    Replace TiXmlDocument load and save functions by PHYSFS
    The TinyXml functions to load and save files don't properly support
    unicode file paths on Windows, so in order to support that properly, I
    saw no other option than to do the actual loading and saving via PHYSFS
    (or to use the Windows API on Windows and retain doc.LoadFile and
    doc.SaveFile on other OSes, but that'd be more complicated and
    unnecessary, we already have PHYSFS, right?).
    
    There are two new functions in FileSystemUtils:
    bool FILESYSTEM_saveTiXmlDocument(const char *name, TiXmlDocument *doc)
    bool FILESYSTEM_loadTiXmlDocument(const char *name, TiXmlDocument *doc)
    
    Any instances of doc.SaveFile(<FULL_PATH>) have been replaced by
    FILESYSTEM_saveTiXmlDocument(<VVVVVV_FOLDER_PATH>, &doc), where
    <FULL_PATH> included the full path to the saves or levels directory,
    and <VVVVVV_FOLDER_PATH> only includes the path relative to the VVVVVV
    directory.
    When loading a document, a TiXmlDocument used to be created with a full
    path in its constructor and doc.LoadFile() would then be called, now a
    TiXmlDocument is constructed with no path name and
    FILESYSTEM_loadTiXmlDocument(<VVVVVV_FOLDER_PATH>, &doc) is called.

--- desktop_version/src/FileSystemUtils.h
+++ desktop_version/src/FileSystemUtils.h
@@ -5,6 +5,8 @@
 #include <vector>
 #include "Game.h"
 
+#include "tinyxml.h"
+
 int FILESYSTEM_init(char *argvZero);
 void FILESYSTEM_deinit();
 
@@ -13,6 +15,8 @@ char *FILESYSTEM_getUserLevelDirectory();
 
 void FILESYSTEM_loadFileToMemory(const char *name, unsigned char **mem, size_t *len);
 void FILESYSTEM_freeMemory(unsigned char **mem);
+bool FILESYSTEM_saveTiXmlDocument(const char *name, TiXmlDocument *doc);
+bool FILESYSTEM_loadTiXmlDocument(const char *name, TiXmlDocument *doc);
 
 growing_vector<std::string> FILESYSTEM_getLevelDirFileNames();
 
