40c3830fb4ad3db8363176dcc35853348b940647
Info Teddy <infoteddyatgitgud@cock.li>
2020-01-12 20:14:07 -0800

    Add destroy(trinkets)
    Apparently they're all internal entity type 7.

--- desktop_version/src/Script.cpp
+++ desktop_version/src/Script.cpp
@@ -160,6 +160,10 @@ void scriptclass::run( KeyPoll& key, Graphics& dwgfx, Game& game, mapclass& map,
 					for (int eni = 0; eni < obj.nentity; eni++)
 						if (obj.entities[eni].rule == 1)
 							obj.entities[eni].active = false;
+				} else if (words[1] == "trinkets") {
+					for (int eti = 0; eti < obj.nentity; eti++)
+						if (obj.entities[eti].type == 7)
+							obj.entities[eti].active = false;
 				}
 			}
 			if (words[0] == "customiftrinkets")
