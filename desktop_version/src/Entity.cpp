v6cord/VVVVVV

commit 0cbd9a2352d39e61dee568167c190efc9c29da62

Add seventh argument to createcrewman, if it is flip spawn a flipped crewmate

desktop_version/src/Entity.cpp
@@ -2238,8 +2238,9 @@ void entityclass::createentity( Game& game, float xp, float yp, int t, float vx 
         entities[k].gravity = true;
         break;
     case 18: // Crew Member (Ship)
+    case 57: // Crew Member (Flipped)
         //This is the scriping crewmember
-        entities[k].rule = 6;
+        entities[k].rule = t == 57 ? 7 : 6;
         entities[k].type = 12; //A special case!
         entities[k].colour = vx;
         if (int(vy) == 0)
@@ -4204,6 +4205,8 @@ void entityclass::animateentities( int _i, Game& game, UtilityClass& help )
                 {
                     entities[_i].drawframe=entities[_i].tile+3;
                 }
+
+                if (entities[_i].rule == 7) entities[_i].drawframe += 6;
 
                 if(entities[_i].onground>0 || entities[_i].onroof>0)
                 {

desktop_version/src/Script.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include "Script.h"
 #include "Graphics.h"
 
@@ -1129,14 +1130,19 @@ void scriptclass::run( KeyPoll& key, Graphics& dwgfx, Game& game, mapclass& map,
 					words[6] = "0";
 				}
 
-				if (ss_toi(words[5]) >= 16)
-				{
-					obj.createentity(game, ss_toi(words[1]), ss_toi(words[2]), 18, r, ss_toi(words[4]), ss_toi(words[5]), ss_toi(words[6]));
-				}
-				else
-				{
-					obj.createentity(game, ss_toi(words[1]), ss_toi(words[2]), 18, r, ss_toi(words[4]), ss_toi(words[5]));
-				}
+                                int ent = 18;
+                                if (words[7] == "flip") {
+                                    ent = 57;
+                                }
+
+                                if (ss_toi(words[5]) >= 16)
+                                {
+                                    obj.createentity(game, ss_toi(words[1]), ss_toi(words[2]), ent, r, ss_toi(words[4]), ss_toi(words[5]), ss_toi(words[6]));
+                                }
+                                else
+                                {
+                                    obj.createentity(game, ss_toi(words[1]), ss_toi(words[2]), ent, r, ss_toi(words[4]), ss_toi(words[5]));
+                                }
 			}
 			else if (words[0] == "changemood")
 			{

desktop_version/src/titlerender.cpp
@@ -91,6 +91,8 @@ growing_vector<std::string> changelog = {
     "- destroy(teleporter)",
     "- inf - like do(x), but an infinite",
     "  amount of times",
+    "- Add seventh argument to createcrewman,",
+    "  if it is flip spawn a flipped crewmate",
 
 };
 

commit 3086a0567f7ca3fa6f66a2e513f4d2661bc27a6e

Move sprite-setting of flipped crewmates to createentity
This fixes the flipgravity() command which broke in
0cbd9a2352d39e61dee568167c190efc9c29da62, because the sprite-setting was
done when updating the entity's animations rather than when the entity
was being created. In other words, that commit changed the sprite of the
crewmate one too many times if they were flipped using flipgravity().

desktop_version/src/Entity.cpp
@@ -2267,6 +2267,8 @@ int entityclass::createentity( Game& game, float xp, float yp, int t, float vx /
             entities[k].dir = p2;
         }
 
+        if (entities[k].rule == 7) entities[k].tile += 6;
+
         entities[k].gravity = true;
         break;
     case 19: // Crew Member (Ship) More tests!
@@ -4206,8 +4208,6 @@ void entityclass::animateentities( int _i, Game& game, UtilityClass& help )
                 {
                     entities[_i].drawframe=entities[_i].tile+3;
                 }
-
-                if (entities[_i].rule == 7) entities[_i].drawframe += 6;
 
                 if(entities[_i].onground>0 || entities[_i].onroof>0)
                 {