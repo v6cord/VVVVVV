b989857b9cb6b96a275ab8df4b37554beca1da06
Info Teddy <infoteddyatgitgud@cock.li>
2020-01-16 22:46:33 -0800

    Allow using Warp Zone gray tileset in editor

--- desktop_version/src/editor.cpp
+++ desktop_version/src/editor.cpp
@@ -4368,6 +4368,10 @@ void editorinput( KeyPoll& key, Graphics& dwgfx, Game& game, mapclass& map, enti
                 {
                     if(ed.level[ed.levx+(ed.levy*ed.maxwidth)].tilecol>=8) ed.level[ed.levx+(ed.levy*ed.maxwidth)].tilecol=0;
                 }
+                else if(ed.level[ed.levx+(ed.levy*ed.maxwidth)].tileset==3)
+                {
+                    if(ed.level[ed.levx+(ed.levy*ed.maxwidth)].tilecol>=7) ed.level[ed.levx+(ed.levy*ed.maxwidth)].tilecol=0;
+                }
                 else
                 {
                     if(ed.level[ed.levx+(ed.levy*ed.maxwidth)].tilecol>=6) ed.level[ed.levx+(ed.levy*ed.maxwidth)].tilecol=0;
