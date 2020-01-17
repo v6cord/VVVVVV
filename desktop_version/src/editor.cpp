3533734a1882a34d52a071ec1f00d0994f15e2c6
leo60228 <iakornfeld@gmail.com>
2020-01-17 17:02:53 -0500

    Use correct tag length in find_tag

--- desktop_version/src/editor.cpp
+++ desktop_version/src/editor.cpp
@@ -5355,7 +5355,7 @@ void replaceAll(std::string& str, const std::string& from, const std::string& to
 
 std::string find_tag(std::string_view buf, std::string_view start, std::string_view end) {
     auto title_tag = buf.find(start);
-    auto title_start = title_tag + 7;
+    auto title_start = title_tag + start.size();
     auto title_close = buf.find(end, title_start);
     auto title_len = title_close - title_start;
     std::string value(buf.substr(title_start, title_len));
