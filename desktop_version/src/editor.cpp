925d6de8f040be3c8c5308a14fff01073817dc31
Info Teddy <infoteddyatgitgud@cock.li>
2020-01-19 21:23:56 -0800

    Made editorclass::absat() not use a pointer interface
    For some reason, FIQ made it do so in
    9d5a2723a37e201a4deb84bf2097ed16b206cb9f. I see no reason to do so, so
    I'm just going to remove the whole pointer interface thing.

--- desktop_version/src/editor.cpp
+++ desktop_version/src/editor.cpp
@@ -1133,7 +1133,7 @@ editorclass::gettiletyp(int x, int y)
 enum tiletyp
 editorclass::getabstiletyp(int x, int y)
 {
-    int tile = absat(&x, &y);
+    int tile = absat(x, y);
     int room = x / 40 + ((y / 30)*ed.maxwidth);
 
     if (tile == 0)
@@ -1165,13 +1165,13 @@ int editorclass::at( int x, int y )
 }
 
 int
-editorclass::absat(int *x, int *y)
+editorclass::absat(int x, int y)
 {
-    if (*x < 0) *x = (*x) +mapwidth*40;
-    if (*y < 0) *y = (*y) +mapheight*30;
-    if (*x >= (mapwidth*40)) *x = (*x) - mapwidth*40;
-    if (*y >= (mapheight*30)) *y = (*y) - mapheight*30;
-    return contents[(*x) + vmult[*y]];
+    if (x < 0) x = x +mapwidth*40;
+    if (y < 0) y = y +mapheight*30;
+    if (x >= (mapwidth*40)) x = x - mapwidth*40;
+    if (y >= (mapheight*30)) y = y - mapheight*30;
+    return contents[x + vmult[y]];
 }
 int editorclass::freewrap( int x, int y )
 {
