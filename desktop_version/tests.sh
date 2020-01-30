780e8881beb1a08ec1881535f72c4ce538b5055d
leo60228 <iakornfeld@gmail.com>
2020-01-30 10:56:00 -0500

    Add tests

---
+++ desktop_version/tests.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+cd "$(dirname "$0")"
+unset DISPLAY
+export XDG_DATA_HOME="$(pwd)/tests"
+export USERPROFILE="$(pwd)/tests"
+source ./build.sh
+if [ ! -f data.zip ]; then
+    wget https://thelettervsixtim.es/makeandplay/data.zip
+fi
+set +e -o pipefail
+failed=0
+for testcase in "$XDG_DATA_HOME"/VVVVVV/levels/*.vvvvvv; do
+    testcase="$(basename "$testcase" .vvvvvv)"
+    echo -n "$testcase -- "
+    if output="$(./VVVVVV-CE -p "$testcase" --headless 2>&1 | tail -n1)"; then
+        echo "PASS"
+    else
+        echo "$output"
+        failed=1
+    fi
+done
+exit "$failed"
