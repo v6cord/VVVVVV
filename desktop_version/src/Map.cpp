7d7434d04fbb2eb399eda7a8d3f9064daff679c6
Info Teddy <infoteddyatgitgud@cock.li>
2020-01-31 15:45:01 -0800

    Fix a -Wmisleading-indentation in warpto()
    It's only misleading indentation if you have 8-space tabs and don't have
    tabs and spaces marked differently in your editor.

--- desktop_version/src/Map.cpp
+++ desktop_version/src/Map.cpp
@@ -1059,8 +1059,8 @@ bool mapclass::leaving_tower(int *rx, int *ry, entityclass &obj) {
 void mapclass::warpto(int rx, int ry , int t, int tx, int ty, Graphics& dwgfx, Game& game, entityclass& obj, musicclass& music)
 {
 	gotoroom(rx, ry, dwgfx, game, obj, music);
-    if (towermode)
-        realign_tower(obj);
+	if (towermode)
+		realign_tower(obj);
 	game.teleport = false;
 	obj.entities[t].xp = tx * 8;
 	obj.entities[t].yp = (ty * 8) - obj.entities[t].h;
