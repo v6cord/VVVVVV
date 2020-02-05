4e37fb80febd63174b564f4bd61fd9413b43face
leo60228 <iakornfeld@gmail.com>
2020-02-05 09:13:34 -0500

    Support passing extra CMake options on build.sh command line

--- desktop_version/build.sh
+++ desktop_version/build.sh
@@ -30,6 +30,7 @@ cmake -G Ninja \
     ${debug:+-DCMAKE_BUILD_TYPE=Debug} \
     ${debug:--DCMAKE_BUILD_TYPE=RelWithDebInfo} \
     ${windows:+-DCMAKE_TOOLCHAIN_FILE=../toolchain.cmake -DCMAKE_MODULE_PATH="$CMAKE_MODULE_PATH"} \
+    "$@" \
     ..
 
 if cmp -s "$timestamp_ref" vce.zip.c; then
