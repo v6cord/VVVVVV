2cf8be4de4c31918a9b60eb9f8e45db121cbb24b
leo60228 <iakornfeld@gmail.com>
2020-02-05 09:49:26 -0500

    Download data.zip in cpack.sh

--- desktop_version/cpack.sh
+++ desktop_version/cpack.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 cd "$(dirname "$0")"
 source ./build.sh
+if [[ ! -f data-zip/data.zip && ! -f data.zip ]]; then
+    mkdir -p data-zip
+    wget -O data-zip/data.zip https://thelettervsixtim.es/makeandplay/data.zip
+fi
+if [ ! -f data.zip ]; then
+    ln -s data-zip/data.zip data.zip
+fi
 cpack
