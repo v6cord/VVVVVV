1592fe1950beffecd555bd28dae159b88467d447
leo60228 <iakornfeld@gmail.com>
2020-02-07 16:01:38 -0500

    Fix non-Switch

--- desktop_version/src/trace.c
+++ desktop_version/src/trace.c
@@ -3,7 +3,17 @@
 #include <inttypes.h>
 
 static FILE *fp_trace;
+#ifdef __SWITCH__
 extern char __code_start;
+
+uintptr_t base(void) {
+    return (uintptr_t) &__code_start;
+}
+#else
+uintptr_t base(void) {
+    return 0;
+}
+#endif
 
 void __attribute__ ((constructor)) trace_begin (void)
 {
@@ -19,12 +29,12 @@ void __attribute__ ((destructor)) trace_end (void)
 
 void __cyg_profile_func_enter (void *func,  void *caller) {
     if(fp_trace != NULL) {
-        fprintf(fp_trace, "e 0x%"PRIxPTR" 0x%"PRIxPTR"\n", ((uintptr_t) func) - ((uintptr_t) &__code_start), ((uintptr_t) caller) - ((uintptr_t) &__code_start));
+        fprintf(fp_trace, "e 0x%"PRIxPTR" 0x%"PRIxPTR"\n", ((uintptr_t) func) - base(), ((uintptr_t) caller) - base());
     }
 }
 
 void __cyg_profile_func_exit (void *func, void *caller) {
     if(fp_trace != NULL) {
-        fprintf(fp_trace, "x 0x%"PRIxPTR" 0x%"PRIxPTR"\n", ((uintptr_t) func) - ((uintptr_t) &__code_start), ((uintptr_t) caller) - ((uintptr_t) &__code_start));
+        fprintf(fp_trace, "x 0x%"PRIxPTR" 0x%"PRIxPTR"\n", ((uintptr_t) func) - base(), ((uintptr_t) caller) - base());
     }
 }
