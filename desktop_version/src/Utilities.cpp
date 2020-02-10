7d53889f8b51bced5187dd525050620bc03ef001
leo60228 <iakornfeld@gmail.com>
2020-02-10 09:48:58 -0500

    Revert to pre-battery-status changes

--- desktop_version/src/Utilities.cpp
+++ desktop_version/src/Utilities.cpp
@@ -1,21 +1,6 @@
 #include <algorithm>
-#include <fstream>
-#include <string>
-#include <optional>
 #include <stdint.h>
-#include <string.h>
 #include "Utilities.h"
-#include <stdio.h>
-
-#ifdef __linux__
-#include <dirent.h>
-#include <limits.h>
-#include "FileSystemUtils.h"
-#elif __SWITCH__
-#include <switch.h>
-#elif _WIN32
-#include <windows.h>
-#endif
 
 uint64_t splitmix64(uint64_t& x) {
     uint64_t z = (x += 0x9e3779b97f4a7c15);
@@ -80,80 +65,3 @@ size_t bsd_strlcpy(char *dst, const char *src, size_t dsize) {
 }
 
 void free_delete::operator()(void* x) { free(x); }
-
-#ifdef __linux__
-static std::optional<std::string> battery_path() {
-    auto dir = opendir("/sys/class/power_supply");
-    if (!dir) {
-        perror("opendir");
-        return {};
-    }
-    struct dirent* entry;
-    while ((entry = readdir(dir)) != nullptr) {
-        if (strncmp(entry->d_name, "BAT", 3) == 0) {
-            std::string path = "/sys/class/power_supply/";
-            path += entry->d_name;
-            return path;
-        }
-    }
-    return {};
-}
-
-int battery_level() {
-    if (auto bat = battery_path()) {
-        *bat += "/capacity";
-        std::ifstream stream(*bat);
-        int level = 100;
-        stream >> level;
-        return level;
-    } else {
-        return 100;
-    }
-}
-
-bool on_battery() {
-    if (auto bat = battery_path()) {
-        *bat += "/status";
-        std::ifstream stream(*bat);
-        std::string status;
-        stream >> status;
-        return status != "Charging";
-    } else {
-        return false;
-    }
-}
-#elif defined(__SWITCH__)
-int battery_level() {
-    psmInitialize();
-    uint32_t level = 100;
-    psmGetBatteryChargePercentage(&level);
-    return (int) level;
-}
-
-bool on_battery() {
-    psmInitialize();
-    ChargerType type;
-    psmGetChargerType(&type);
-    return type == ChargerType_None;
-}
-#elif defined(_WIN32)
-int battery_level() {
-    SYSTEM_POWER_STATUS status;
-    if (!GetSystemPowerStatus(&status)) return 100;
-    return (int) status.BatteryLifePercent;
-}
-
-bool on_battery() {
-    SYSTEM_POWER_STATUS status;
-    if (!GetSystemPowerStatus(&status)) return false;
-    return status.ACLineStatus == 0;
-}
-#elif !defined(__APPLE__)
-int battery_level() {
-    return 100;
-}
-
-bool on_battery() {
-    return false;
-}
-#endif
