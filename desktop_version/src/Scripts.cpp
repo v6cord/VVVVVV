bffc64586d3de3e410128282afc0276b3ece9762
Info Teddy <infoteddyatgitgud@cock.li>
2020-02-09 23:05:58 -0800

    Add addvar()/setvar()/ifvar()/if() to don't-parse-textboxlike code
    Note that if() is just an alias for ifvar(), but I had to include a
    check for if(), too.
    
    Anyways, whenever those commands were added, somebody forgot to update
    the don't-parse-textboxlike-line code to make sure the simplified parser
    skips over the line after the command if applicable.

--- desktop_version/src/Scripts.cpp
+++ desktop_version/src/Scripts.cpp
@@ -352,7 +352,10 @@ void scriptclass::load(std::string t)
             || words[0] == "drawtext"
             || words[0] == "text"
             || words[0] == "createroomtext"
-            || words[0] == "customactivityzone") {
+            || words[0] == "customactivityzone"
+            || (words[0] == "addvar" && words[2] != "")
+            || (words[0] == "setvar" && words[2] != "")
+            || ((words[0] == "ifvar" || words[0] == "if") && words[4] != "")) {
               // Don't parse the next line if it is a textbox-like line
               add(script.customscript[i]);
               i++;
