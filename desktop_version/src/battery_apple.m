7fa73050c1f4c8831b37cb304cc6fdcd276273f1
leo60228 <iakornfeld@gmail.com>
2020-02-10 09:40:30 -0500

    Attempt to fix

--- desktop_version/src/battery_apple.m
+++ desktop_version/src/battery_apple.m
@@ -4,52 +4,55 @@
 #import <Foundation/Foundation.h>
 
 int battery_level() {
+    int percentage = 100;
+
     CFTypeRef powerSourceInfo = IOPSCopyPowerSourcesInfo();
     CFArrayRef powerSources = IOPSCopyPowerSourcesList(powerSourceInfo);
 
-    CFDictionaryRef powerSource = NULL;
-
     long numberOfSources = CFArrayGetCount(powerSources);
-    if(numberOfSources == 0) {
-        return 100;
-    } else {
-        powerSource = IOPSGetPowerSourceDescription(powerSourceInfo, CFArrayGetValueAtIndex(powerSources, 0));
+    if(numberOfSources != 0) {
+        CFDictionaryRef powerSource = IOPSGetPowerSourceDescription(powerSourceInfo, CFArrayGetValueAtIndex(powerSources, 0));
 
         const void *psValue;
         int curCapacity = 0;
         int maxCapacity = 0;
-        int percentage;
 
-        psValue = CFDictionaryGetValue(powerSource, CFSTR(kIOPSCurrentCapacityKey));
+        psValue = CFDictionaryGetValue(powerSource, @kIOPSCurrentCapacityKey);
         CFNumberGetValue((CFNumberRef)psValue, kCFNumberSInt32Type, &curCapacity);
 
-        psValue = CFDictionaryGetValue(powerSource, CFSTR(kIOPSMaxCapacityKey));
+        psValue = CFDictionaryGetValue(powerSource, @kIOPSMaxCapacityKey);
         CFNumberGetValue((CFNumberRef)psValue, kCFNumberSInt32Type, &maxCapacity);
 
         percentage = (int)((double)curCapacity/(double)maxCapacity * 100);
-
-        return percentage;
     }
+
+    CFRelease(powerSourceInfo);
+    CFRelease(powerSources);
+
+    return percentage;
 }
 
 _Bool on_battery() {
+    _Bool onBattery = 0;
+
     CFTypeRef powerSourceInfo = IOPSCopyPowerSourcesInfo();
     CFArrayRef powerSources = IOPSCopyPowerSourcesList(powerSourceInfo);
 
     CFDictionaryRef powerSource = NULL;
 
     long numberOfSources = CFArrayGetCount(powerSources);
-    if(numberOfSources == 0) {
-        return 0;
-    } else {
+    if(numberOfSources != 0) {
         powerSource = IOPSGetPowerSourceDescription(powerSourceInfo, CFArrayGetValueAtIndex(powerSources, 0));
 
-        NSString str = CFDictionaryGetValue(powerSource, CFSTR(kIOPSPowerSourceStateKey));
+        NSString str = CFDictionaryGetValue(powerSource, @kIOPSPowerSourceStateKey);
 
         if ([str isEqualToString:@kIOPSBatteryPowerValue]) {
-            return 1;
-        } else {
-            return 0;
+            onBattery = 1;
         }
     }
+
+    CFRelease(powerSourceInfo);
+    CFRelease(powerSources);
+
+    return onBattery;
 }
