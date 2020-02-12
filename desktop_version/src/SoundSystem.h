37cbb276d1666bd733027c1f26026a201286ec85
leo60228 <iakornfeld@gmail.com>
2020-02-12 14:01:19 -0500

    Dummy out SDL_Mixer on 3DS

--- desktop_version/src/SoundSystem.h
+++ desktop_version/src/SoundSystem.h
@@ -1,10 +1,37 @@
 #ifndef SOUNDSYSTEM_H
 #define SOUNDSYSTEM_H
 
+#ifndef NO_SDL_MIXER
 #if defined(STRICT_SDL_PATH) && !defined(__ANDROID__)
 	#include <SDL2/SDL_mixer.h>
 #else
 	#include <SDL_mixer.h>
+#endif
+#else
+#define Mix_Chunk void
+#define Mix_FadeInMusic 0
+#define Mix_FadeOutChannel 0
+#define Mix_FadeOutMusic 0
+#define Mix_FadingMusic 0
+#define Mix_FreeChunk 0
+#define Mix_FreeMusic 0
+#define Mix_GetError 0
+#define Mix_HaltChannel 0
+#define Mix_HaltMusic 0
+#define Mix_LoadMUS 0
+#define Mix_LoadMUS_RW 0
+#define Mix_LoadWAV_RW 0
+#define Mix_Music void
+#define Mix_OpenAudio 0
+#define Mix_Pause 0
+#define Mix_PauseMusic 0
+#define Mix_PlayChannel 0
+#define Mix_PlayingMusic 0
+#define Mix_PlayMusic 0
+#define Mix_Resume 0
+#define Mix_ResumeMusic 0
+#define Mix_Volume 0
+#define Mix_VolumeMusic 0
 #endif
 
 class MusicTrack
