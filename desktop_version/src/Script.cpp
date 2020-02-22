07ee94a61265066473ff66684c89b4adc59ff692
Misa <infoteddyatgitgud@cock.li>
2020-02-21 16:55:01 -0800

    Add ifcrewmates(n,script)
    I thought it was pretty weird to have an iftrinkets() and an ifcoins(),
    but no ifcrewmates().

--- desktop_version/src/Script.cpp
+++ desktop_version/src/Script.cpp
@@ -2505,6 +2505,14 @@ void scriptclass::run( KeyPoll& key, Graphics& dwgfx, Game& game, mapclass& map,
 					position--;
 				}
 			}
+			else if (words[0] == "ifcrewmates")
+			{
+				if (game.crewmates >= ss_toi(words[1]))
+				{
+					call("custom_"+words[2]);
+					position--;
+				}
+			}
 			else if (words[0] == "ifcoins")
 			{
 				if (game.coins >= ss_toi(words[1]))
