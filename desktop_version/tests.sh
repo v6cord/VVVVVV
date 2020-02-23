7d15cf06464fd407c1665c4afdfd74858742cc9d
leo60228 <iakornfeld@gmail.com>
2020-02-23 18:17:31 -0500

    Add timeout for tests
    Hopefully, this will prevent a repeat of the 6 hour tests...

--- desktop_version/tests.sh
+++ desktop_version/tests.sh
@@ -17,13 +17,13 @@ for testcase in "$XDG_DATA_HOME"/VVVVVV/levels/*.{vvvvvv,zip}; do
     testcase="$(basename "$testcase" .vvvvvv)"
     testcase="${testcase%.zip}"
     echo -n "$testcase -- "
-    if output="$(./VVVVVV-CE -p "$testcase" --headless --quiet 2>&1)"; then
+    if output="$(timeout 5s ./VVVVVV-CE -p "$testcase" --headless --quiet 2>&1)"; then
         echo "PASS"
     else
         echo "$output"
         if command -v gdb >/dev/null 2>/dev/null; then
             echo -n "BACKTRACE: "
-            gdb -batch -ex "run" -ex "bt" --args ./VVVVVV-CE -p "$testcase" --headless --quiet
+            timeout 5s gdb -batch -ex "run" -ex "bt" --args ./VVVVVV-CE -p "$testcase" --headless --quiet
         fi
         failed=1
     fi
