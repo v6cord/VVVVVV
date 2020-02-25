6ec6a48558ab14d3cecfa6acc488a13d9f44e074
Misa <infoteddyatgitgud@cock.li>
2020-02-24 18:12:46 -0800

    Add %player_onroof% and %player_onfloor% to the changelog

--- desktop_version/src/titlerender.cpp
+++ desktop_version/src/titlerender.cpp
@@ -261,6 +261,7 @@ growing_vector<std::string> changelog = {
     "- Built-in variables (%deaths%,",
     "  %player_x%, %player_y%,",
     "  %gravitycontrol%, %room_x%, %room_y%,",
+    "  %player_onroof%, %player_onfloor%,",
     "  %trinkets%, %crewmates%, %coins%,",
     "  %battery_level%, %on_battery%,",
     "  %unix_time%, %hhmmss_time%)",
