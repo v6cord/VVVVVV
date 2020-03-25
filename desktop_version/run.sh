6f5f05511ce559fd966a438580f4dfd523f83a39
leo60228 <iakornfeld@gmail.com>
2020-03-25 08:51:08 -0400

    Fix arguments in run.sh

--- desktop_version/run.sh
+++ desktop_version/run.sh
@@ -3,16 +3,19 @@ set -e
 
 cd "$(dirname "$0")"
 
+ARGS=("$@")
+set --
+
 source build.sh # source build.sh so we get its argument parsing for free!
 
 export DISPLAY="${DISPLAY:-:0}" # if you're developing over ssh, set $DISPLAY
 
 if [[ "$windows" == "1" ]]; then
     if command -v wine >&/dev/null; then
-        exec wine ./VVVVVV-CE.exe "$@" # use wine if we have it
+        exec wine ./VVVVVV-CE.exe "${ARGS[@]}" # use wine if we have it
     else
-        exec ./VVVVVV-CE.exe "$@" # assume we can execute PEs if we don't (either we're on Windows or the user has binfmt-misc set up)
+        exec ./VVVVVV-CE.exe "${ARGS[@]}" # assume we can execute PEs if we don't (either we're on Windows or the user has binfmt-misc set up)
     fi
 else
-    exec ./VVVVVV-CE "$@" # if we're not on windows, we can run it directly
+    exec ./VVVVVV-CE "${ARGS[@]}" # if we're not on windows, we can run it directly
 fi
