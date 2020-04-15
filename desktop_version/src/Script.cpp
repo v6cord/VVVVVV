d844785ea93be800cf6f29a6cbb6cf698b49a8b5
Misa <infoteddyatgitgud@cock.li>
2020-04-14 19:01:45 -0700

    Fix some missing parentheses
    Yeah this was kind of my fault. I was a bit lazy and rushed when
    merging.

--- desktop_version/src/Script.cpp
+++ desktop_version/src/Script.cpp
@@ -1196,9 +1196,9 @@ void scriptclass::run() {
                 if (words[0] == "flag") {
                     if (ss_toi(words[1]) >= 0 && ss_toi(words[1]) < 1000) {
                         if (words[2] == "on") {
-                            obj.flags[ss_toi(words[1]] = true;
+                            obj.flags[ss_toi(words[1])] = true;
                         } else if (words[2] == "off") {
-                            obj.flags[ss_toi(words[1]] = false;
+                            obj.flags[ss_toi(words[1])] = false;
                         }
                     }
                 }
