97f6aa6bf82813e14b0c6415fac426097669d5e1
leo60228 <leo@60228.dev>
2020-06-12 13:06:34 -0400

    Lua textbox object

--- desktop_version/src/LuaScript.cpp
+++ desktop_version/src/LuaScript.cpp
@@ -8,35 +8,15 @@
 #include <utility>
 #include <stdexcept>
 
-lua_script::lua_script(std::string name, std::vector<std::string> contents) {
-    for (auto& line : contents) {
-        text += line;
-        text += "\n";
-    }
+struct textbox {
+    std::string text;
+    int x = 0;
+    int y = 0;
 
-    lua.open_libraries(sol::lib::base, sol::lib::string, sol::lib::math, sol::lib::table);
-    add_functions();
+    textbox(std::string t) : text(t) {}
+    textbox() = default;
 
-    sol::protected_function::set_default_handler(lua["error_handler"]);
-
-    thread = sol::thread::create(lua);
-    sol::protected_function func = lua.load(text).get<sol::protected_function>();
-    sol::state_view thread_state = thread.state();
-    coroutine = sol::coroutine(thread_state, func);
-}
-
-void lua_script::add_functions() {
-    lua.set_function("error_handler", [](std::string msg) {
-        script_exception ex(msg, true);
-        std::cerr << ex.what() << std::endl;
-        throw ex;
-    });
-
-    lua["delay"] = sol::yielding([this](unsigned int time) {
-        delay = time;
-    });
-
-    lua["say"] = sol::yielding([this](int x, int y, std::string text) {
+    void show() {
         std::stringstream ss(text);
         std::string line;
         std::getline(ss, line, '\n');
@@ -55,7 +35,59 @@ void lua_script::add_functions() {
             key.isDown(KEYBOARD_UP) || key.isDown(KEYBOARD_DOWN)) {
             game.jumpheld = true;
         }
+    }
 
+    void say() {
+        // TODO: squeak
+        show();
+    }
+};
+
+lua_script::lua_script(std::string name, std::vector<std::string> contents) {
+    for (auto& line : contents) {
+        text += line;
+        text += "\n";
+    }
+
+    lua.open_libraries(sol::lib::base, sol::lib::string, sol::lib::math, sol::lib::table);
+    add_functions();
+
+    sol::protected_function::set_default_handler(lua["error_handler"]);
+
+    thread = sol::thread::create(lua);
+    sol::protected_function func = lua.load(text, name).get<sol::protected_function>();
+    sol::state_view thread_state = thread.state();
+    coroutine = sol::coroutine(thread_state, func);
+}
+
+void lua_script::add_functions() {
+    lua.set_function("error_handler", [](std::string msg) {
+        script_exception ex(msg, true);
+        std::cerr << ex.what() << std::endl;
+        throw ex;
+    });
+
+    lua["delay"] = sol::yielding([this](unsigned int time) {
+        delay = time;
+    });
+
+    sol::usertype<textbox> t = lua.new_usertype<textbox>("textbox",
+            sol::constructors<textbox(), textbox(std::string)>());
+    t["text"] = &textbox::text;
+    t["x"] = &textbox::x;
+    t["y"] = &textbox::y;
+    t["show"] = sol::yielding([this](textbox t) {
+        t.show();
+        endtext = true;
+    });
+    t["say"] = sol::yielding([this](sol::object obj) {
+        textbox t;
+        if (obj.is<textbox>()) {
+            t = obj.as<textbox>();
+        } else {
+            t = textbox(obj.as<std::string>());
+        }
+        t.say();
         endtext = true;
     });
 }
