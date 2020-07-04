97058c2ddb9b3a10890825c43585c4f83cec1bd5
AllyTally <mariomario13245@gmail.com>
2020-07-04 18:36:38 -0300

    add progress bar in terminal
    unsure if thisll be kept, but its nice to have since theres no other visual feedback

--- desktop_version/src/FileSystemUtils.cpp
+++ desktop_version/src/FileSystemUtils.cpp
@@ -303,6 +303,35 @@ size_t write_data(void* ptr, size_t size, size_t nmemb, PHYSFS_File* stream) {
     return written;
 }
 
+int progress_func(void* ptr, double TotalToDownload, double NowDownloaded,
+    double TotalToUpload, double NowUploaded)
+{
+    if (TotalToDownload <= 0.0) {
+        return 0;
+    }
+    double fractiondownloaded = NowDownloaded / TotalToDownload;
+
+    int chars_to_display = round(fractiondownloaded * 40);
+
+    // Create the bar
+    int ii = 0;
+    printf("Downloading level... %3.0f%% [", fractiondownloaded * 100);
+    // Fill in what's already done
+    for (; ii < chars_to_display; ii++) {
+        printf("#");
+    }
+    // remaining part (spaces)
+    for (; ii < 40; ii++) {
+        printf(" ");
+    }
+    // and back to line begin - do not forget the fflush to avoid output buffering problems!
+    printf("]\r");
+    fflush(stdout);
+    // if you don't return 0, the transfer will be aborted - see the documentation
+
+    return 0;
+}
+
 bool FILESYSTEM_downloadFile(const char* name, const char* url) {
     CURL* curl;
     PHYSFS_File* fp;
@@ -314,9 +343,12 @@ bool FILESYSTEM_downloadFile(const char* name, const char* url) {
         curl_easy_setopt(curl, CURLOPT_URL, url);
         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
         curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
+        curl_easy_setopt(curl, CURLOPT_NOPROGRESS, FALSE);
+        curl_easy_setopt(curl, CURLOPT_PROGRESSFUNCTION, progress_func);
         res = curl_easy_perform(curl);
         curl_easy_cleanup(curl);
         PHYSFS_close(fp);
+        printf("\n");
     }
     else
     {
